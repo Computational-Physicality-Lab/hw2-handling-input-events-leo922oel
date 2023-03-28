3b03375915f0ab48c0f23c5ebdab1ecbbe93462a
leo922oel <leo922oel@gmail.com>
2023-03-28 18:15:56 +0800

    impl. click unfinished

--- input.js
+++ input.js
@@ -6,3 +6,23 @@ to handle mouse, touch and possible other events.
 You will certainly need a large number of global variables to keep track of the current modes and states
 of the interaction.
 */
+const target = document.querySelectorAll(".target");
+
+for (var id=0; id < target.length; id++) {
+    var bg = target[id];
+    console.log(bg);
+    target[id].addEventListener(
+        "click", 
+        (e) => {
+            // var style = getComputedStyle(target[id]);
+            if (target[id].background-color == 'blue') {
+                target[id].removeAttribute('background-color');
+                target.setAttribute('background-color', 'red');
+            } else {
+                target[id].removeAttribute('background-color');
+                target[id].setAttribute('background-color', 'blue');
+            }
+        },
+        false
+    );
+}
