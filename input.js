6161ab411d3ae93fc461bd8bffbf7bf8020179ed
leo922oel <leo922oel@gmail.com>
2023-03-30 20:44:06 +0800

    impl. handler, debug click

--- input.js
+++ input.js
@@ -6,23 +6,61 @@ to handle mouse, touch and possible other events.
 You will certainly need a large number of global variables to keep track of the current modes and states
 of the interaction.
 */
-const target = document.querySelectorAll(".target");
+const target = document.getElementsByClassName("target");
 
 for (var id=0; id < target.length; id++) {
-    var bg = target[id];
-    console.log(bg);
-    target[id].addEventListener(
-        "click", 
-        (e) => {
-            // var style = getComputedStyle(target[id]);
-            if (target[id].background-color == 'blue') {
-                target[id].removeAttribute('background-color');
-                target.setAttribute('background-color', 'red');
-            } else {
-                target[id].removeAttribute('background-color');
-                target[id].setAttribute('background-color', 'blue');
-            }
-        },
-        false
-    );
+    console.log(target[id]);
+    target[id].addEventListener("click", SelectObj);
+    target[id].addEventListener("mousedown", MouseHandler);
+    target[id].addEventListener("dbclick", doubleclick);
+}
+
+/////// need debug
+function SelectObj(e) {
+    if (e.type === "click") {
+        e.preventDefault();
+        document.div = this;
+        console.log(document.div.class);
+        if (document.div.style.backgroundColor == 'rgb(255, 0, 0)') {
+            console.log("blue to red");
+            document.div.style.backgroundColor = 'rgb(0, 0, 255)';
+        } else {
+            console.log("red to blue");
+            document.div.style.backgroundColor = 'rgb(255, 0, 0)';
+        }
+    }
+}
+function MouseHandler(e) {
+    if (e.type === "mousedown") {
+        e.preventDefault();
+        document.div = this;
+        document.offset = {x: e.offsetX, y:e.offsetY};
+        document.addEventListener("mousemove", MouseHandler);
+        document.addEventListener("mouseup", MouseHandler);
+    } else if (e.type === "mousemove") {
+        document.div.style.left = e.clientX - document.offset.x + "px";
+        document.div.style.top = e.clientY - document.offset.y + "px";
+
+    } else if (e.type === "mouseup") {
+        document.removeEventListener("mousemove", MouseHandler);
+        document.removeEventListener("mouseup", MouseHandler);
+    }
+}
+
+function doubleclick(e) {
+    if (e.type === "dbclick") {
+        // selectObj(e);
+        e.preventDefault();
+        document.div = this;
+        document.offset = {x: e.offsetX, y:e.offsetY};
+        document.addEventListener("mousemove", doubleclick);
+        document.addEventListener("mouseup", doubleclick);
+    } else if (e.type === "mousemove") {
+        document.div.style.left = e.clientX - document.offset.x + "px";
+        document.div.style.top = e.clientY - document.offset.y + "px";
+
+    } else if (e.type === "mouseup") {
+        document.removeEventListener("mousemove", doubleclick);
+        document.removeEventListener("mouseup", doubleclick);
+    }
 }
