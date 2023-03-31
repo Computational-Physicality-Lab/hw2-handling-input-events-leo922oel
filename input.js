4b854b5c0ad50a2ab18c51e7e079bbb0dda3b2f3
leo922oel <leo922oel@gmail.com>
2023-03-31 14:55:58 +0800

    old ver.

--- input.js
+++ input.js
@@ -6,21 +6,28 @@ to handle mouse, touch and possible other events.
 You will certainly need a large number of global variables to keep track of the current modes and states
 of the interaction.
 */
-const target = document.querySelectorAll(".target");
-
+// const target = document.querySelectorAll(".target");
+var target = document.querySelectorAll(".target");
 for (var id=0; id < target.length; id++) {
-    var bg = target[id];
-    console.log(bg);
+    var style = getComputedStyle(target[id]);
+    console.log(target[id]);
     target[id].addEventListener(
         "click", 
         (e) => {
             // var style = getComputedStyle(target[id]);
-            if (target[id].background-color == 'blue') {
-                target[id].removeAttribute('background-color');
-                target.setAttribute('background-color', 'red');
-            } else {
-                target[id].removeAttribute('background-color');
-                target[id].setAttribute('background-color', 'blue');
+            if (style.backgroundColor == "rgb(255, 0, 0)") {
+                // target[id].removeAttribute('background-color');
+                e.currentTarget.style.backgroundColor = 'blue';
+                console.log(getComputedStyle(e.currentTarget))
+                // target.setAttribute('background-color', 'red');
+                // style.setProperty("background-color", "blue");
+                // target[id].innerHTML = 'red'
+            } else if (style.backgroundColor == "rgb(0, 0, 255)") {
+                // target[id].removeAttribute('background-color');
+                // target[id].setAttribute('background-color', 'blue');
+                console.log("rrrrrr");
+                e.currentTarget.style.backgroundColor = 'red';
+                // style.setProperty("background-color", "red");
             }
         },
         false
